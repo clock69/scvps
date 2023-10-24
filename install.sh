6a68316861a8b2808d442af1fc6797b27f7025e4
clock69 <147904587+clock69@users.noreply.github.com>
2023-10-24 16:09:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Dat
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/clock69/Perizinan/main/IP > /root/tmp
+    curl -sS https://raw.githubusercontent.com/clock69/izin/main/IP > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
@@ -19,9 +19,9 @@ BURIQ () {
     done
     rm -f  /root/tmp
 }
-# https://raw.githubusercontent.com/clock69/Perizinan/main/IP
+# https://raw.githubusercontent.com/clock69/izin/main/IP
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/clock69/Perizinan/main/IP | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS https://raw.githubusercontent.com/clock69/izin/main/IP | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
